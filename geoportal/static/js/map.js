5250a322b475a0cf926fe97e4e90e4b72530951c
Your Name <you@example.com>
2020-05-01 16:17:09 +0300

    fixed some bugs

--- geoportal/static/js/map.js
+++ geoportal/static/js/map.js
@@ -131,7 +131,7 @@ function addLayer(data, queryName) {
     feature.addTo(map);
     var token = makeId(10)
     queryLayers[token] = feature
-    var layerHtml = '<div class="row custom-control custom-checkbox justify-content-between">'
+    var layerHtml = '<div class="row custom-control custom-checkbox justify-content-between ml-2 mt-3">'
     layerHtml += '<button type="button" name="add-custom-layer" data-token="' + token + '" class="btn btn-xs p-0 pb-3 mr-2 pr-3" data-toggle="modal" data-target="#add-layer-query"><span class="fa fa-plus mr-2"></span></button>'
     layerHtml += '<input type="checkbox" id="checkbox-' + token + '" class="custom-control-input" onclick="toggleQueryLayer(' + "'" + token + "'" +')">'
     layerHtml += '<label class="custom-control-label normal" style="font-size: 17px" for="checkbox-' + token + '">' + queryName + '</label></div>'
